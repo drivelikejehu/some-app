237c82749dd006759a409607169a36f8a9d808a0
drivelikejehu <pollyfactor@hotmail.com>
2021-06-24 19:06:30 -0400

    mvp expo act with firebase

---
+++ API/firebaseMethods.js
@@ -0,0 +1,35 @@
+import "firebase/firestore";
+import { Alert } from "react-native";
+import firebase from "firebase/app";
+
+export async function registration(email, password, lastName, firstName) {
+  try {
+    await firebase.auth().createUserWithEmailAndPassword(email, password);
+    const currentUser = firebase.auth().currentUser;
+
+    const db = firebase.firestore();
+    db.collection("users").doc(currentUser.uid).set({
+      email: currentUser.email,
+      lastName: lastName,
+      firstName: firstName,
+    });
+  } catch (err) {
+    Alert.alert("There is something wrong!!!", err.message);
+  }
+}
+
+export async function signIn(email, password) {
+  try {
+    await firebase.auth().signInWithEmailAndPassword(email, password);
+  } catch (err) {
+    Alert.alert("Something's not right!", err.message);
+  }
+}
+
+export async function loggingOut() {
+  try {
+    await firebase.auth().signOut();
+  } catch (err) {
+    Alert.alert("This is wrong!", err.message);
+  }
+}
